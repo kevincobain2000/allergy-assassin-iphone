e4f5678b28b09c6236b475ba32ced8fe5e1449d1
Matt Jackson <me@mattjackson.eu>
2013-08-30 17:02:47 +0100

    finished about screen

--- Allergy Assassin/AboutViewController.m
+++ Allergy Assassin/AboutViewController.m
@@ -25,37 +25,33 @@
                                   , [logo frame].size.width))];
     logo.center = CGPointMake([[self view] frame].size.width/2, logo.center.y);
     
-    UITextView *url = [[UITextView alloc] initWithFrame:CGRectZero];
-    [url setTextAlignment:NSTextAlignmentCenter];
+    UITextView *url = [[UITextView alloc] initWithFrame:
+        CGRectMake(0, 0,
+                   [self view].frame.size.width, 80)];
     [url setText:@"http://allergyassassin.com"];
     [url setTextAlignment:NSTextAlignmentCenter];
-    [url setCenter:CGPointMake([[self view] center].x, [logo frame].size.height+10)];
+    [url setCenter:CGPointMake([[self view] center].x, [logo frame].size.height+45)];
     url.editable = NO;
+    [url setDataDetectorTypes:UIDataDetectorTypeLink];
     
     UITextView *description = [[UITextView alloc]
-        initWithFrame:CGRectMake([url frame].origin.x,
-                                 [url frame].origin.y + 10,
-                                 120, 30)];
-    [description setTextAlignment:NSTextAlignmentRight];
-    [description setText:@"by Matt Jackson"];
-    description.editable = NO;
-    
-    UITextView *version = [[UITextView alloc]
-        initWithFrame:CGRectMake([url frame].origin.y + [url frame].size.width-50,
-                                 [description frame].origin.y,
-                                 50,
+        initWithFrame:CGRectMake(0,
+                                 [url frame].origin.y + 25,
+                                 [self view].frame.size.width,
                                  30)];
-    [version setText:[NSString stringWithFormat:@"v%@",  [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]]];
-    version.editable = NO;
+    [description setTextAlignment:NSTextAlignmentCenter];
+    [description setText:[NSString stringWithFormat:@" v%@ by Matt Jackson",
+                          [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]]];
+    description.editable = NO;
     
     
     UIImageView *infoIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"info.png"]];
-    [infoIcon setFrame:CGRectMake(0, [description frame].origin.y + 30,
+    [infoIcon setFrame:CGRectMake(0, [description frame].origin.y + 35,
                                   [[self view] frame].size.width , 30)];
     [infoIcon setContentMode:UIViewContentModeScaleAspectFit];
     
     UITextView *infoText = [[UITextView alloc]
-        initWithFrame:CGRectMake(0,[infoIcon frame].origin.y + 35,
+        initWithFrame:CGRectMake(0,[infoIcon frame].origin.y + 30,
                                  [[self view] frame].size.width, 120)];
     [infoText setText:[AllergyAssassinSearch disclaimer]];
     [infoText setTextAlignment:NSTextAlignmentCenter];
@@ -64,7 +60,6 @@
     [[self view] addSubview:logo];
     [[self view] addSubview:url];
     [[self view] addSubview:description];
-    [[self view] addSubview:version];
     [[self view] addSubview:infoIcon];
     [[self view] addSubview:infoText];
 }
